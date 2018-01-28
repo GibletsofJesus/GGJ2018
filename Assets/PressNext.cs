5b3b816e7ae19b5b800cd9a748f3e5d8081a82a6
dialcforchris <christopher2.mugridge@live.uwe.ac.uk>
2018-01-28 13:22:29 +0000

    ui stuff

---
+++ Assets/PressNext.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PressNext : MonoBehaviour
+{
+    public Text text;
+    public static PressNext instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+            Destroy(this);
+    }
+
+    public void Died()
+    {
+        text.gameObject.SetActive(true);
+        text.text = "You Died /n Press Cross to continue";
+    }
+      public void DoneLevel()
+    {
+        text.gameObject.SetActive(true);
+        text.text = "Transmission valid /n Press Cross to continue";
+    }
+
+    private void Update()
+    {
+        if((Controller.Button()&ControllerButton.CROSS)!=0)
+        {
+            text.gameObject.SetActive(false);
+        }
+    }
+}
