15db3a1b9afc62bc1abb37e6ed8a630c1c649a88
dialcforchris <christopher2.mugridge@live.uwe.ac.uk>
2018-01-28 11:38:58 +0000

    changed jumping a bit

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -9,7 +9,7 @@ public class Player : MonoBehaviour
 
     public Sprite[] m_sprites;
     int frame;
-    
+
     private int lives = 3;
     public float maxHorizVelocity = 1.0f;
     public bool wallJumping = false;
@@ -23,68 +23,89 @@ public class Player : MonoBehaviour
     Vector2 rayPos;
     bool dj = true;
     SpriteRenderer spRenderer;
+    float fallMulti = 2.5f;
+    float jumpMulti = 2;
+    float stompMulti;
+    float jumpVel = 5;
+
     private void Awake()
     {
-        instance=this;
+        instance = this;
         jumpsRemaining = totalJumps;
         body = GetComponent<Rigidbody2D>();
         collider = GetComponent<BoxCollider2D>();
         rayPos = new Vector2(transform.position.x, collider.bounds.min.y);
         spRenderer = GetComponent<SpriteRenderer>();
+        stompMulti = fallMulti *2;
     }
 
     private void Update()
-    {  
+    {
+        if (body.velocity.y < 0)
+        {
+            body.velocity += Vector2.up * Physics2D.gravity.y * ((slamming ? stompMulti : fallMulti) - 1) * Time.deltaTime;
+        }
+        else if (body.velocity.y > 0 && !Controller.GetButton(ControllerButton.CROSS))
+        {
+            body.velocity += Vector2.up * Physics2D.gravity.y * (jumpMulti - 1) * Time.deltaTime;
+        }
+
         if (transform.position.y < -5)
         {
             Objective.instance.RestartLevel();
         }
         rayPos = new Vector2(transform.position.x, collider.bounds.min.y);
-        if (wallJumping) {
+        if (wallJumping)
+        {
             wallJumpTimer += Time.deltaTime;
-            if (wallJumpTimer >= 0.1f) {
+            if (wallJumpTimer >= 0.1f)
+            {
                 wallJumping = false;
             }
         }
-        else {
-            if (Controller.LX() != 0 && !slamming) {
-                body.velocity += 3 * Time.deltaTime * new Vector2(25*Controller.LX(), 0);
+        else
+        {
+            if (Controller.LX() != 0 && !slamming)
+            {
+                body.velocity += 3 * Time.deltaTime * new Vector2(25 * Controller.LX(), 0);
             }
-            else if (!slamming){
+            else if (!slamming)
+            {
                 body.velocity -= 5 * Time.deltaTime * new Vector2(body.velocity.x, 0);
             }
-            if (Math.Abs(body.velocity.x) > maxHorizVelocity) {
-                body.velocity = new Vector2(Mathf.Clamp(body.velocity.x, -maxHorizVelocity, maxHorizVelocity) , body.velocity.y);
-            }
-            if((Controller.Button()&ControllerButton.CIRCLE)!=0&&Controller.LX()!=0)
+            if (Math.Abs(body.velocity.x) > maxHorizVelocity)
             {
-                body.AddForce(new Vector2(800* Controller.LX(), 0));
+                body.velocity = new Vector2(Mathf.Clamp(body.velocity.x, -maxHorizVelocity, maxHorizVelocity), body.velocity.y);
+            }
+            if ((Controller.Button() & ControllerButton.CIRCLE) != 0 && Controller.LX() != 0)
+            {
+                body.AddForce(new Vector2(800 * Controller.LX(), 0));
             }
             spRenderer.flipX = body.velocity.x < 0;
         }
         if (OnGround())
         {
-            if (Mathf.Abs(body.velocity.x)>.5f)
+            if (Mathf.Abs(body.velocity.x) > .5f)
             {
-                spRenderer.sprite=m_sprites[Mathf.RoundToInt(frame/10)];
-                frame = frame<40 ? frame+1 : 0;
+                spRenderer.sprite = m_sprites[Mathf.RoundToInt(frame / 10)];
+                frame = frame < 40 ? frame + 1 : 0;
             }
             else
             {
-                spRenderer.sprite=m_sprites[5+Mathf.RoundToInt(Time.time%1)];
+                spRenderer.sprite = m_sprites[5 + Mathf.RoundToInt(Time.time % 1)];
             }
         }
         else if (OnWall() && !slamming)
         {
             if (Controller.LX() > 0.25f && !spRenderer.flipX)
-             spRenderer.sprite=m_sprites[7];
+                spRenderer.sprite = m_sprites[7];
             else if (Controller.LX() < -0.25f && spRenderer.flipX)
-             spRenderer.sprite=m_sprites[7];
+                spRenderer.sprite = m_sprites[7];
             else
-            spRenderer.sprite=m_sprites[4];
+                spRenderer.sprite = m_sprites[4];
         }
-            else
-            spRenderer.sprite=m_sprites[4];
+        else
+            spRenderer.sprite = m_sprites[4];
         Jump();
     }
 
@@ -98,8 +119,8 @@ public class Player : MonoBehaviour
     #region Jumping Shit
     bool OnGround()
     {
-        Debug.DrawLine(rayPos,rayPos+(Vector2.down*(0.05f+Mathf.Abs(body.velocity.y/100))),Color.red,0.1f);
-        if (Physics2D.Raycast(rayPos, Vector2.down, 0.05f+Mathf.Abs(body.velocity.y/100)))
+        Debug.DrawLine(rayPos, rayPos + (Vector2.down * (0.05f + Mathf.Abs(body.velocity.y / 100))), Color.red, 0.1f);
+        if (Physics2D.Raycast(rayPos, Vector2.down, 0.05f + Mathf.Abs(body.velocity.y / 100)))
         {
             return true;
         }
@@ -108,45 +129,48 @@ public class Player : MonoBehaviour
 
     bool OnWall()
     {
-        Vector3 vec=new Vector3(spRenderer.flipX ? collider.bounds.min.x : collider.bounds.max.x, transform.position.y,0);
-        Debug.DrawLine(vec,vec+(spRenderer.flipX ? -transform.right*0.05f: transform.right*0.05f),Color.green,0.1f);
-        if (Physics2D.Raycast(vec, spRenderer.flipX ? -transform.right : transform.right, 0.05f)) 
+        Vector3 vec = new Vector3(spRenderer.flipX ? collider.bounds.min.x : collider.bounds.max.x, transform.position.y, 0);
+        Debug.DrawLine(vec, vec + (spRenderer.flipX ? -transform.right * 0.05f : transform.right * 0.05f), Color.green, 0.1f);
+        if (Physics2D.Raycast(vec, spRenderer.flipX ? -transform.right : transform.right, 0.05f))
         {
-             return true;
+            return true;
         }
         return false;
     }
 
     //For breaking boxes
-    public float lastyVel=0;
+    public float lastyVel = 0;
     void LateUpdate()
     {
-        lastyVel=body.velocity.y;
+        lastyVel = body.velocity.y;
     }
 
     void Jump()
     {
         if (OnGround())
         {
-            slamming=false;
+            slamming = false;
             jumpsRemaining = totalJumps;
         }
         if ((Controller.Button() & ControllerButton.DOWN) != 0 && jumpsRemaining != totalJumps)
+        {
+            if ((Controller.ButtonDown() & ControllerButton.SQUARE) != 0)
             {
-                if ((Controller.ButtonDown() & ControllerButton.SQUARE) != 0)
-             {   StartCoroutine(SlamPause(dj));
+                StartCoroutine(SlamPause(dj));
                 dj = true;
             }
-            }
+        }
         if ((Controller.ButtonDown() & ControllerButton.CROSS) != 0)
-        {      
+        {
             if ((Controller.Button() & (spRenderer.flipX ? ControllerButton.LEFT : ControllerButton.RIGHT)) != 0)
             {
                 if (spRenderer.flipX)
                 {
                     if (Physics2D.Raycast(new Vector2(collider.bounds.min.x, transform.position.y), -transform.right, 0.1f))
                     {
-                        body.AddForce(jumpForce + new Vector2(400, 0));
+                        //                        body.AddForce(jumpForce + new Vector2(400, 0));
+                        body.velocity = (Vector2.up * jumpVel) + ((Vector2.right) * jumpVel);
+
                         wallJumping = true;
                         wallJumpTimer = 0.0f;
                         jumpsRemaining = 1;
@@ -156,40 +180,43 @@ public class Player : MonoBehaviour
                 {
                     if (Physics2D.Raycast(new Vector2(collider.bounds.max.x, transform.position.y), transform.right, 0.1f))
                     {
-                        body.AddForce(jumpForce + new Vector2(-400, 0));
+                        //body.AddForce(jumpForce + new Vector2(-400, 0));
+                        body.velocity = (Vector2.up * jumpVel) + ((-Vector2.right) * jumpVel);
                         wallJumping = true;
                         wallJumpTimer = 0.0f;
                         jumpsRemaining = 1;
                     }
                 }
             }
-            if (!wallJumping) {
-                if (jumpsRemaining != 0) {
-                    body.AddForce(jumpForce);
+            if (!wallJumping)
+            {
+                if (jumpsRemaining != 0)
+                {
+                    body.velocity = Vector2.up * jumpVel;
                     jumpsRemaining--;
                 }
             }
         }
     }
 
-    public bool slamming=false;
+    public bool slamming = false;
     IEnumerator SlamPause(bool _dj)
     {
-        slamming=true;
+        slamming = true;
         body.velocity = Vector2.zero;
         body.simulated = false;
         float lerpy = 0;
-        body.constraints =RigidbodyConstraints2D.None;
-        while (lerpy<1)
+        body.constraints = RigidbodyConstraints2D.None;
+        while (lerpy < 1)
         {
-            lerpy += Time.deltaTime*4;
-            transform.rotation = Quaternion.Euler(0, 0, lerpy* 360);
+            lerpy += Time.deltaTime * 4;
+            transform.rotation = Quaternion.Euler(0, 0, lerpy * 360);
             yield return new WaitForEndOfFrame();
         }
-        body.constraints =RigidbodyConstraints2D.FreezeRotation;
+        body.constraints = RigidbodyConstraints2D.FreezeRotation;
         transform.rotation = Quaternion.Euler(0, 0, 0);
         body.simulated = true;
-        body.AddForce(-jumpForce * 3);
+        body.velocity = -Vector2.up * jumpVel;
     }
     public void Death()
     {
@@ -199,15 +226,4 @@ public class Player : MonoBehaviour
     }
     #endregion
 
-    private void OnCollisionEnter2D(Collision2D _col)
-    {
-
-    }
-}
-enum JumpState
-{
-    Once = 0,
-    Twice = 1,
-    Slam = 2,
-    Nope = 3,    
 }
