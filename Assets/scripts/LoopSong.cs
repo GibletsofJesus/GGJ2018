9219002257275035735c6cf74b4429e64f284be2
dialcforchris <christopher2.mugridge@live.uwe.ac.uk>
2018-01-28 16:46:46 +0000

    loop

---
+++ Assets/scripts/LoopSong.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LoopSong : MonoBehaviour {
+
+    public  AudioClip clip
+        ;
+    AudioSource source;
+    public static LoopSong instance = null;
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+
+        source.clip = clip;
+        source.loop = true;
+        source.Play();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
