b42c2ed415e6f04c4e2bf6d3d5c837a58147b44e
dialcforchris <christopher2.mugridge@live.uwe.ac.uk>
2018-01-28 16:08:51 +0000

    breaking shit

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -132,10 +132,24 @@ public class Player : MonoBehaviour
     bool OnGround()
     {
         Debug.DrawLine(rayPos, rayPos + (Vector2.down * (0.05f + Mathf.Abs(body.velocity.y / 100))), Color.red, 0.1f);
-        if (Physics2D.Raycast(rayPos, Vector2.down, 0.05f + Mathf.Abs(body.velocity.y / 100)))
+        RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.down, 0.05f + Mathf.Abs(body.velocity.y / 100));
+        if (hit)
         {
+            if (slamming)
+            {
+                if (hit.collider.gameObject.GetComponent<IGetHit>() != null)
+                {
+                    hit.collider.gameObject.GetComponent<IGetHit>().GotHit();
+                }
+                slamming = false;
+            }
             return true;
         }
+        //if (Physics2D.Raycast(rayPos, Vector2.down, 0.05f + Mathf.Abs(body.velocity.y / 100)))
+        //{
+        //    slamming = false;
+        //    return true;
+        //}
         return false;
     }
 
@@ -154,15 +168,7 @@ public class Player : MonoBehaviour
     public float lastyVel = 0;
     void LateUpdate()
     {
-        RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.down, 0.05f + Mathf.Abs(body.velocity.y / 100));
-        if (hit && slamming)
-        {
-            if (hit.collider.gameObject.GetComponent<IGetHit>() != null)
-            {
-                //hit.collider.gameObject.GetComponent<IGetHit>().GotHit();
-            }         
-            slamming = false;
-        } 
+       
         
         lastyVel = body.velocity.y;
     }
