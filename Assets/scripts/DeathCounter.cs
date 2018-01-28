cca70e6d84c3826b9dd0533ec4cd19badbc57b20
dialcforchris <christopher2.mugridge@live.uwe.ac.uk>
2018-01-28 15:52:54 +0000

    counter

--- Assets/scripts/DeathCounter.cs
+++ Assets/scripts/DeathCounter.cs
@@ -6,17 +6,20 @@ public class DeathCounter : MonoBehaviour
 {
     int deathcount = 0;
     public Text counter;
-    string[] rating = new string[4] {"Awesome!", "OK", "Amateur", "YOU SUCK"};
     private void Awake()
     {
         deathcount = PlayerPrefs.GetInt("deaths");
     }
+    private void OnEnable()
+    {
+       Player.instance.Dead += UpdateDeaths;
+    }
+ 
     // Use this for initialization
+
     void Start ()
     {
-        int temp = deathcount * PlayerPrefs.GetInt("Level");
-        int ratingNum = (temp< 5? 0: temp<20? 1: temp <30? 2 : 3);
-        counter.text = "Death Count : " + deathcount + "\nRating : " + rating[ratingNum];	
+        counter.text = "Death Count : " + deathcount;// + "\nRating : " + rating[ratingNum];	
 	}
 	
     void UpdateDeaths()
